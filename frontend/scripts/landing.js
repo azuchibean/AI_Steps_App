89dd998331c4c2ce624852fedc3c4f09f82032cf
Emily0816Lin <122253001+Emily0816Lin@users.noreply.github.com>
2024-11-21 00:26:46 -0800

    remove duplicate URL declaration

--- frontend/scripts/landing.js
+++ frontend/scripts/landing.js
@@ -1,7 +1,3 @@
-// const API_BASE_URL = "http://127.0.0.1:8000";                    // in dev branch for testing
-const API_BASE_URL = "https://coral-app-3m7bi.ondigitalocean.app";  // in main branch for deployment
-
-
 function loadLandingPageContent(userData) {
     const contentDiv = document.getElementById("content"); // Get the content div
     contentDiv.style.display = "block"; // Ensure the content div is visible
