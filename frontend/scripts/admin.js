ff63e38a84ea215581925a9809c335051e8f3fe3
Angela Yu <ayu107@my.bcit.ca>
2024-11-23 04:00:50 -0800

    user strings admin

--- frontend/scripts/admin.js
+++ frontend/scripts/admin.js
@@ -1,3 +1,5 @@
+
+
 function loadAdminPageContent(){
     const contentDiv = document.getElementById("content"); // Get the content div
     contentDiv.style.display = "block"; // Ensure the content div is visible
@@ -7,10 +9,18 @@ function loadAdminPageContent(){
     contentDiv.appendChild(adminPageMessageElement);
 
     //load Section 1: endpoint stats 
+    document.getElementById("endpoint-stats-title").textContent = messages.endpointStatsTitle;
+    document.getElementById("endpointMethod").textContent = messages.endpointMethodTitle;
+    document.getElementById("endPointEndpoint").textContent =  messages.endpointEndpointTitle;
+    document.getElementById("endpointRequests").textContent = messages.endpointRequestsTitle;
     loadEndpointStats();
 
     //load Section 2: api usage stats
-    loadApiUsageStats();
+    document.getElementById("user-api-consumption-title").textContent = messages.userAPIConsumptionTitle;
+    document.getElementById("usageUsername").textContent = messages.usageUsernameTitle;
+    document.getElementById("usageEmail").textContent = messages.usageEmailTitle;
+    document.getElementById("usageTotalRequests").textContent = messages.usageTotalRequestsTitle;
+    loadApiUsageStats(); 
  
 };
 
@@ -81,3 +91,8 @@ function renderApiUsageStats(stats) {
         tableBody.appendChild(row);
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadAdminPageContent();
+    console.log("loading admin page content");
+});
